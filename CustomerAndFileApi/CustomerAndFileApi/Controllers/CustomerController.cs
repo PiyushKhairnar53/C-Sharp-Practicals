6dced4bd6f0dfe0530530a32f5bb563cb32138be
piyush khairnar <piyushkhairnar53@gmail.com>
2023-03-08 17:19:47 +0530

    CustomerController modification committed

--- CustomerAndFileApi/CustomerAndFileApi/Controllers/CustomerController.cs
+++ CustomerAndFileApi/CustomerAndFileApi/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Xml.Linq;
 using CustomerAndFileApi.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -14,19 +15,23 @@ namespace CustomerAndFileApi.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-
         private static List<Customer> customerList = new List<Customer>()
         {
-            new Customer() {Id = 1, Name = "ABC", Locations = new List<Locations>{new Locations(){custId = 1,Id = 1, City = "Pune", Address = "Bavdhan"} } },
+            new Customer() {Id = 1, Name = "ABC", Locations = new List<Locations>{new Locations(){Id = 1, City = "Pune", Address = "Bavdhan"} } },
         };
 
         /// <summary>
         /// Get all customer details.
         /// </summary>
         [HttpGet]
-        public ActionResult<IEnumerable<Customer>> GetAllCustomers()
+        public IActionResult GetAllCustomers()
         {
-            return customerList;
+            if (customerList != null)
+            {
+                return Ok(customerList);
+            }
+            
+            return NoContent();
         }
 
         /// <summary>
@@ -35,16 +40,27 @@ namespace CustomerAndFileApi.Controllers
         /// <param name="id">Enter Id of Customer.</param>
         [HttpGet("{id}")]
         [SwaggerResponse(StatusCodes.Status200OK, "Returns the customer.", typeof(Customer))]
-        public ActionResult<Customer> GetCustomerById(int id)
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Customer not found.", typeof(Customer))]
+        public IActionResult GetCustomerById(int id)
         {
-            var customer = customerList.FirstOrDefault(c => c.Id == id);
-
-            if (customer == null)
+            try
             {
-                return NotFound();
+                var customer = customerList.FirstOrDefault(c => c.Id == id);
+                if (customer != null)
+                {
+                    return Ok(customer);
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-
-            return customer;
+            catch (Exception exception) 
+            {
+                return BadRequest(exception.Message);
+            }
+            
+            return NoContent();
         }
 
         /// <summary>
@@ -52,12 +68,19 @@ namespace CustomerAndFileApi.Controllers
         /// </summary>
         /// <param name="customer">Enter Customer</param>
         [HttpPost]
-        public IActionResult PostCustomerDetails(Customer customer)
+        public IActionResult AddCustomerDetails(Customer customer)
         {
             int newCustomerId = customerList.Max(c => c.Id) + 1;
             customer.Id = newCustomerId;
-            customerList.Add(customer);
-            return CreatedAtAction(nameof(customer.Id), new { id = customer.Id }, customer);
+            try
+            {
+                customerList.Add(customer);
+                return CreatedAtAction(nameof(customer.Id), new { id = customer.Id }, customer);
+            }
+            catch (Exception exception) 
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
         /// <summary>
@@ -66,6 +89,7 @@ namespace CustomerAndFileApi.Controllers
         /// <param name="id">Enter ID of customer to update.</param>
         /// <param name="newCustomer">Enter new details of customer</param>
         [HttpPut("{id}")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Customer not found.", typeof(Customer))]
         [SwaggerResponse(StatusCodes.Status204NoContent, "Customer successfully updated.")]
         public IActionResult UpdateCustomerDetails(int id, Customer newCustomer)
         {
@@ -78,12 +102,13 @@ namespace CustomerAndFileApi.Controllers
             if (!string.IsNullOrEmpty(newCustomer?.Name))
             {
                 customer.Name = newCustomer.Name;
+                return Ok();
             }
             if (newCustomer?.Locations != null && newCustomer.Locations.Any())
             {
                 customer.Locations = newCustomer.Locations;
+                return Ok();
             }
-
             return NoContent();
         }
 
@@ -92,6 +117,7 @@ namespace CustomerAndFileApi.Controllers
         /// </summary>
         /// <param name="id">Enter Customer Id to delete.</param>
         [HttpDelete("{id}")]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Customer not found.", typeof(Customer))]
         [SwaggerResponse(StatusCodes.Status204NoContent, "Customer successfully deleted.")]
         public IActionResult DeleteCustomerDeatils(int id)
         {
@@ -101,9 +127,66 @@ namespace CustomerAndFileApi.Controllers
             {
                 return BadRequest("Cannot delete a customer which have locations");
             }
+            try
+            {
+                if (customerList.Contains(customer))
+                {
+                    customerList.Remove(customer);
+                    return Ok(customer);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception exception) 
+            {
+                return BadRequest(exception.Message);
+            }
+            return NoContent();
+        }
 
-            customerList.Remove(customer);
-            
+        /// <summary>
+        /// Delete the Location of Customer
+        /// </summary>
+        /// <param name="id">Enter Customer Id.</param>
+        /// <param name="locationId">Enter Location Id to delete Location.</param>
+        [Route("location")]
+        [HttpDelete]
+        [SwaggerResponse(StatusCodes.Status200OK, "Location Deleted.", typeof(Locations))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Location not found.", typeof(Locations))]
+        public IActionResult DeleteCustomerLocation(int id,int locationId)
+        {
+            foreach (Customer customer in customerList) 
+            {
+                if (customer.Id == id) 
+                {
+                    List<Locations> oldCustomerLocations = new List<Locations>();
+
+                    foreach (Locations location in customer.Locations) 
+                    {
+                        oldCustomerLocations.Add(location);
+                    }
+                    try
+                    {
+                        var itemToRemove = oldCustomerLocations.Single(r => r.Id == locationId);
+                        if (oldCustomerLocations.Contains(itemToRemove))
+                        {
+                            oldCustomerLocations.Remove(itemToRemove);
+                            customer.Locations = oldCustomerLocations;
+                            return Ok(itemToRemove);
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                    catch(Exception ex) 
+                    {
+                        return BadRequest("Exception:"+ex.Message);
+                    }
+                }
+            }
             return NoContent();
         }
 
