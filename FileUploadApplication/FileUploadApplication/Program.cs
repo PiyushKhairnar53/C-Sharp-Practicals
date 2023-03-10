27c194374ef4595324da5c9b719541931bea78e4
piyush khairnar <piyushkhairnar53@gmail.com>
2023-03-10 17:21:38 +0530

    File upload application committed

---
+++ FileUploadApplication/FileUploadApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileUploadApplication
+{
+    internal class FileUploadClass
+    {
+        public static void Main(string[] args) 
+        {
+
+            MultipleFiles multipleFiles = new MultipleFiles();
+            multipleFiles.CopyMultipleFiles();
+
+            FileUploadProgress fileUploadProgress = new FileUploadProgress();
+            fileUploadProgress.FileUploadProgressMethod();
+
+        } 
+    }
+}
