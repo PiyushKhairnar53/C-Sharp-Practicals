1f0b3004db808622f6c4d586259c36b60342a7ba
piyush khairnar <piyushkhairnar53@gmail.com>
2023-03-16 12:11:37 +0530

    CustomerLocationRPApi committed

---
+++ CustomerLocationRPApi/CustomerLocationRPApi/Program.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+using CustomerLocationRPApi.Services.Repositories;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Customer and Location Repository Pattern Api",
+        Description = "An ASP.NET Core Web API for managing Customers and Locations",
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+});
+
+builder.Services.AddSingleton<ICustomerRepository, CustomerLocationRepository>();
+builder.Services.AddSingleton<ICustomerLocationRepository, CustomerLocationRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
